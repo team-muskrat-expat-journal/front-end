7632e4b49b7800eedf1c083e08d4c586e9442be8
unknown <tompsherman@gmail.com>
2020-11-13 17:48:24 -0500

    scaffolding

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Team 'Skrat Expat Journal</h1>
+    </div>
+  );
+};
+
+export default App;
