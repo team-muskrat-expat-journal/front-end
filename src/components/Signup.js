bc0da2a0824b99d2caf4c7a9fedc67eef2e8630a
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-11-16 13:52:50 -0800

    added yup valid and signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
 
 function Signup() {
   const [form, setForm] = useState({
@@ -7,29 +8,70 @@ function Signup() {
     password: "",
     terms: false,
   });
+
+  const [disabled, setDisabled] = useState(true);
+
+  const change = (event) => {
+    const { checked, value, name, type } = event.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    setForm({ ...form, [name]: valueToUse });
+  };
+  const schema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    email: yup.string().required("Email is required"),
+    password: yup
+      .string()
+      .required("Password is requred")
+      .min(6, "Password is required"),
+    terms: yup.boolean().oneOf([true], "You must give away your data"),
+  });
+  useEffect(() => {
+    schema.isValid(form).then((valid) => setDisabled(!valid));
+  }, [form]);
+
   return (
     <>
       <h1>Sign up!</h1>
       <p>What is needed, - name, email, terms, password, 2nd pass, terms</p>
       <form>
         <label>
-          Your Name <input name="name" type="text" value="name" />
+          Your Name{" "}
+          <input
+            onChange={change}
+            name="name"
+            type="text"
+            placeholder="Your Name"
+          />
         </label>
         <br></br>
         <label>
-          Your Email <input name="email" type="email" />
+          Your Email{" "}
+          <input
+            onChange={change}
+            name="email"
+            type="email"
+            placeholder="Your Email"
+          />
         </label>
         <br></br>
         <label>
-          Password <input name="password" type="password" />
+          Password{" "}
+          <input
+            onChange={change}
+            name="password"
+            type="password"
+            placeholder="Your Password"
+          />
         </label>
         <br></br>
         <label>
           Terms and Conditions
-          <input name="terms" type="checkbox" />
+          <input onChange={change} name="terms" type="checkbox" />
         </label>
         <br></br>
-        <button type="submit">Submit</button>
+        <button disabled={disabled} type="submit">
+          Submit
+        </button>
       </form>
     </>
   );
