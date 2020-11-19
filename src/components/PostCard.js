0aa6c41b3e3857d4bc9080242f5949aa1d990cbe
djviodes <djviodes26@gmail.com>
2020-11-18 21:37:35 -0800

    Added Justins PR to this code to avoid the huge merge conflict.

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { editPost, deletePost } from '../actions/PostsAction';
 
 const initialPost = {
+  id: "",
   name: "",
   date: "",
   location: "",
@@ -14,41 +15,95 @@ const initialPost = {
 }
 
 const PostCard = (props) => {
-  const { id, title, image } = props.post;
+  const { id, name, date, location, imageURL, notes, rating, role } = props;
   const [editing, setEditing] = useState(false);
-  const [editPost, setEditPost] = useState(initialPost);
+  const [postToEdit, setPostToEdit] = useState(initialPost);
 
-  const deleteEntry = event => {
+  const postEdit = (edit) => {
+    setEditing(true);
+    setPostToEdit(id);
+  };
+
+  const postDelete = (event) => {
     event.preventDefault();
     props.deletePost(id);
   };
 
-  const editEntry = () => {
-    setEditing(true);
-    setEditPost(props.post);
-  }
-
-  const onSubmit = event => {
+  const save = (event) => {
     event.preventDefault();
-    props.editPost(id, editPost);
-  }
+    props.postEdit(props.post);
+  };
 
   return (
-    <div className="post-card">
-      <img src={image} alt={title} />
-      <p>{title}</p>
-      <button onClick={deleteEntry}>Delete Post</button>
-      <button onClick={editEntry}>Edit Post</button>
-      <br />
-      {editing && (
-        <form onSubmit={onSubmit}>
-
-          <div>
-            <button type='submit'>Save</button>
-            <button onClick={() => setEditing(false)}>Cancel</button>
-          </div>
-        </form>
-      )}
+    <div className="post card">
+      <div className="image-container">
+        <img src={imageURL} alt={imageURL} />
+      </div>
+      <div className="card-info">
+        <h2>{name}</h2>
+        <p>{date}</p>
+        <p>{location}</p>
+        <p>{notes}</p>
+        <p>{rating}</p>
+        <p>{role}</p>
+        <button onClick={postEdit}>Edit</button>
+        <button onClick={postDelete}>Delete</button>
+        <br />
+        <br />
+        <br />
+        <br />
+        {editing && (
+          <form onSubmit={save}>
+            <legend>Edit Post</legend>
+            <ul>
+              <label>
+                Name:&nbsp;
+                <input
+                  onChange={(evt) =>
+                    setPostToEdit({ ...postToEdit, name: evt.target.value })
+                  }
+                />
+              </label>
+              <label>
+                Date:&nbsp;
+                <input
+                  onChange={(evt) =>
+                    setPostToEdit({ ...postToEdit, date: evt.target.value })
+                  }
+                />
+              </label>
+              <label>
+                Location:&nbsp;
+                <input
+                  onChange={(evt) =>
+                    setPostToEdit({ ...postToEdit, location: evt.target.value })
+                  }
+                />
+              </label>
+              <label>
+                Notes:&nbsp;
+                <input
+                  onChange={(evt) =>
+                    setPostToEdit({ ...postToEdit, notes: evt.target.value })
+                  }
+                />
+              </label>
+              <label>
+                Rating:&nbsp;
+                <input
+                  onChange={(evt) =>
+                    setPostToEdit({ ...postToEdit, rating: evt.target.value })
+                  }
+                />
+              </label>
+            </ul>
+            <div>
+              <button type="submit">save</button>
+              <button onClick={() => setEditing(false)}>cancle</button>
+            </div>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
